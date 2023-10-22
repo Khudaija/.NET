ebf66c0b33780afafa7542815b36e3a1d1b19c7c
Khudaija <86610468+Khudaija@users.noreply.github.com>
2023-10-22 20:38:07 +0530

    Update Basic_CS.cs

--- Basic_CS.cs
+++ Basic_CS.cs
@@ -29,3 +29,82 @@ using System;
             }
         }
     }
+
+//Variables
+
+using System;
+namespace Variables{
+    public class Program{
+        public static void Main(string[] args){
+            /*const int Age = 22; // unchangeable and read-only
+            Age = 23; //error */
+
+            /*char sWord = I; // its used for single character
+            Console.WriteLine(sWord);*/
+            
+            int Age = 22; // float, double
+            string Name = "Khudaija"; 
+            string SrName = " Pinjari";
+            Console.WriteLine("My Name is " + Name + SrName + " and I am " + Age + " years old."); 
+        }
+    }
+}
+
+// DataType
+
+using System;
+namespace DataType{
+    public class Program{
+        public static void Main(string[] args){
+            int Age = 22;
+            Console.WriteLine(Age);
+            double ExactAge = 22.1D;
+            Console.WriteLine(ExactAge);
+            string Name = "Khudaija";
+            Console.WriteLine(Name);
+            char SrName = 'P';
+            Console.WriteLine(SrName);
+            bool myBool = true;
+            Console.WriteLine(myBool);
+        }
+    }
+}
+
+// Type Casting
+
+using System;
+
+namespace TypeCasting{
+    public class Program{
+        public static void Main(string[] args){
+            
+            //implicit casting - done automatically when passing a smaller size type to a larger size type
+            
+            /*int age = 22;
+            double Dage = age;
+            Console.WriteLine(age);
+            Console.WriteLine(Dage);*/
+            
+            //explicit castring - manually by placing the type in parentheses in front of the value
+            
+            /*double age = 22.1;
+            int myage = (int) age;
+            Console.WriteLine(age);
+            Console.WriteLine(myage); */
+            
+            /* Type Conversion Method - convert data types explicitly by using built-in methods, 
+            such as Convert.ToBoolean, Convert.ToDouble, Convert.ToString, Convert.ToInt32 (int) and Convert.ToInt64 (long) */
+            
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+            
+        }
+    }
+}
+
