57b6a058d7a302954633c19fcdef067a46d7e1a6
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-10-25 19:11:32 +0530

    Create if_else_loop.cs

---
+++ if_else_loop.cs
@@ -0,0 +1,23 @@
+// if, else if & else....
+
+using System;
+
+namespace myApp{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Enter your age ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            
+            if(Age==18){
+                Console.WriteLine("Make Your Voter ID Card.");
+            }
+            else if(Age >= 18){
+                Console.WriteLine("Give Your Valuable Vote.");
+            }
+            else{
+                Console.WriteLine("Your Age is invalid for Vote.");
+            }
+            
+        }
+    }
+}
