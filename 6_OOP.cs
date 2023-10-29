dd587fc859eb58f0a30c15656819b86ee1f4bf3e
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-10-29 20:49:16 +0530

    Update 6_OOP.cs

--- 6_OOP.cs
+++ 6_OOP.cs
@@ -236,3 +236,71 @@ namespace OOP{
     }
 }
 
+// Properties (Get and Set) and Encapsulation
+/* 
+=>Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must:
+
+- declare fields/variables as private
+- provide public get and set methods, through properties, to access and update the value of a private field 
+
+=> private variables can only be accessed within the same class (an outside class has no access to it). However, sometimes we need to access them - and it can be done with properties
+
+=> A property is like a combination of a variable and a method, and it has two methods: a get and a set method
+*/
+
+using System;
+
+namespace OOP{
+    class Person{
+        private string name;
+        
+        public string Name{
+            get {return name;}
+            set {name = value;}
+        }
+    }
+    
+    class Program{
+        static void Main(string[] args){
+            
+            Person myObj = new Person();
+            myObj.Name = "SK";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
+// Properties (Get and Set) and Encapsulation
+/* 
+=>Encapsulation, is to make sure that "sensitive" data is hidden from users. To achieve this, you must:
+
+- declare fields/variables as private
+- provide public get and set methods, through properties, to access and update the value of a private field 
+
+=> private variables can only be accessed within the same class (an outside class has no access to it). 
+However, sometimes we need to access them - and it can be done with properties
+
+=> A property is like a combination of a variable and a method, and it has two methods: a get and a set method
+*/
+
+using System;
+
+namespace OOP{
+    class Person{
+        private string name;
+        
+        public string Name{
+            get {return name;}
+            set {name = value;}
+        }
+    }
+    
+    class Program{
+        static void Main(string[] args){
+            
+            Person myObj = new Person();
+            myObj.Name = "SK";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
+
