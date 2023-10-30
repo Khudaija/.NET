0cb0fc8c1325ec2b4f1a06af12f11167324d6776
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-10-30 20:33:36 +0530

    Update 7_enum.cs

--- 7_enum.cs
+++ 7_enum.cs
@@ -94,3 +94,34 @@ namespace OOP{
     }
 }
 
+// Enum in a switch statement
+
+using System;
+
+namespace OOP{
+    class Program{
+        
+        enum Level{
+            Low,
+            Medium,
+            High
+        }
+        
+        static void Main(string[] args){
+            Level myVar = Level.Medium;
+            switch(myVar){
+                case Level.Low:
+                    Console.WriteLine("Low Level");
+                    break;
+                    
+                case Level.Medium:
+                    Console.WriteLine("Medium Level");
+                    break;
+                    
+                case Level.High:
+                    Console.WriteLine("High Level");
+                    break;
+            }
+        }
+    }
+}
