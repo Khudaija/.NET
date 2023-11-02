ba142d85b31d043a222fe1a89e79ef03f8d8d277
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-11-02 18:37:39 +0530

    Update 10_exampe.cs

--- 10_exampe.cs
+++ 10_exampe.cs
@@ -2,13 +2,39 @@
 
 using System;
 
-namespace OOP{
+namespace pattern{
     class Program{
         static void Main(string[] args){
             
             for(int i = 0; i<4; i++){
                 for(int j = 0; j<4; j++){
                     Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+// Pattern
+
+using System;
+
+namespace Pattern{
+    class Program{
+        static void Main(string[] args){
+            
+            int n = 4;
+            int m = 4;
+            
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(i == 0 || j ==0 || i == n-1 || j == m-1){
+                        Console.Write("*");
+                    }
+                    else{
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
             }
