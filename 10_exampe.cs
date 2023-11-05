c25f7c78d6e5a8b7e6b62e092269ace989a7850f
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-11-05 20:57:06 +0530

    Update 10_exampe.cs

--- 10_exampe.cs
+++ 10_exampe.cs
@@ -41,3 +41,23 @@ namespace Pattern{
         }
     }
 }
+
+//star pattern 
+
+using System;
+
+namespace pattern{
+    class Program{
+        static void Main(string[] args){
+            
+            int n = 5; 
+            
+            for(int i=0; i<n; i++){
+                for(int j=0; j<=i; j++){
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
