f59d964d017042b36069d6ef0cea7a395b555484
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-11-14 20:04:01 +0530

    Update 11_ComplexPatterns.cs

--- 11_ComplexPatterns.cs
+++ 11_ComplexPatterns.cs
@@ -91,3 +91,38 @@ namespace Pattern{
     }
 }
 
+// pattern
+
+using System;
+
+namespace Pattern{
+    class Program{
+        static void Main(string[] args){
+            int n = 5;
+            
+            for(int i = 1; i<=n; i++){
+                for(int j=1; j<=n-i; j++){
+                    Console.Write("   ");
+                }
+                int sum = 2*(i-1);
+                for(int j=2; j<=sum; j++){
+                    Console.Write(" * ");
+                }
+                
+                Console.WriteLine();
+            }
+            
+            for(int i=n; i>=1; i--){
+                for(int j=1; j<=n-i; j++){
+                    Console.Write("   ");
+                }
+                int sum = 2*(i-1);
+                for(int j=2; j<=sum; j++){
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
