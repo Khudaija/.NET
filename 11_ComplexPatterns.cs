a2959dd41585ccc4e539300c7e393125272e64f7
Computer Engineer <86610468+Khudaija@users.noreply.github.com>
2023-11-14 19:44:05 +0530

    Update 11_ComplexPatterns.cs

--- 11_ComplexPatterns.cs
+++ 11_ComplexPatterns.cs
@@ -65,3 +65,29 @@ namespace pattern{
     }
 }
 
+// star pattern
+
+using System;
+
+namespace Pattern{
+    class Program{
+        static void Main(string[] args){
+            int n = 5;
+            
+            for(int i=1; i <= n; i++){
+                for(int j=1; j<=n-i; j++){
+                    Console.Write(" ");
+                }
+                for(int j=i; j>=1; j--){
+                    Console.Write(j );
+                }
+                for(int j=2; j<=i;j++){
+                    Console.Write(j );
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
