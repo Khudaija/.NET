c7f58995264a09fd6d7962d03d2f06cf0c8913a6
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2023-11-19 19:27:27 +0530

    Update 12_practices_problems.cs

--- 12_practices_problems.cs
+++ 12_practices_problems.cs
@@ -13,3 +13,20 @@ namespace Practices{
     }
 }
 
+// print the sum of two numbers  Convert.ToInt32
+
+using System;
+
+namespace Practices{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Enter first number: ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(firstNum + secondNum);
+        }
+    }
+}
+
