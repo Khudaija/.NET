6fc88cb0cadb4d0bff01661ec1051401fb44ec84
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2023-11-19 19:13:14 +0530

    Create 12_practices_problems.cs

---
+++ 12_practices_problems.cs
@@ -0,0 +1,15 @@
+//  print Hello and your name in a separate line
+
+using System;
+
+namespace Practices{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Eneter Name: ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Hello");
+            Console.WriteLine(name);
+        }
+    }
+}
+
