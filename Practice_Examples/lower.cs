19d94817983c34181cb91919353a8ac55c79819b
Khudaija Pinjari <86610468+Khudaija@users.noreply.github.com>
2023-12-20 19:59:09 +0530

    Create lower.cs

---
+++ Practice_Examples/lower.cs
@@ -0,0 +1,12 @@
+// convert a given string into lowercase
+
+using System;
+
+namespace Program{
+    class Problem{
+        static void Main(string[] args){
+            string line = "I Am A Girl.";
+            Console.Write(line.ToLower());
+        }
+    }
+}
